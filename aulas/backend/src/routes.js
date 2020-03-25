f6967d19b73d2f74ad3da85a9a8f526a0af710b6
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-25 02:07:33 -0300

    create IncidentController

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
