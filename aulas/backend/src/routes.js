3747d7ff4b9ec257efc39d0b27d3a67d23c6b3a2
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-25 01:53:51 -0300

    create OngController

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
-
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res) => {
-    const ongs = await connection('ongs').select('*');
+const OngController = require('./controllers/OngController');
 
-    return res.json(ongs);
-});
-
-routes.post('/ongs', async (req, res) => {
-    const { name, email, whatsapp, city, uf } = req.body;
-    
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
-
-    res.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
