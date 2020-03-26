1c37e192e905f627846d994eded14d84bc5b2c83
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 11:08:17 -0300

    passing children props

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -3,7 +3,9 @@ import Header from './Header';
 
 function App() {
   return (
-    <Header title="Semana OmniStack" />
+    <Header title="Semana OmniStack">
+      Conteúdo na children
+    </Header>
   );
 }
 
--- aulas/frontend/src/Header.js
+++ aulas/frontend/src/Header.js
@@ -4,6 +4,7 @@ function Header(props) {
     return (
         <header>
             <h1>{props.title}</h1>
+            {props.children}
         </header>
     );
 }
