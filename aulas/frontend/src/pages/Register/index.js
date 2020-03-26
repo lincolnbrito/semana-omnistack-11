600138238860a77d657514fe3950527f798563a1
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 20:39:47 -0300

    use history on register

--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -15,6 +15,8 @@ export default function Register() {
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
+    const history = useHistory();
+
     async function handleRegister(e) {
         e.preventDefault();
 
@@ -29,6 +31,9 @@ export default function Register() {
         try {
             const response = await api.post('ongs', data);
             alert(`Seu ID de acesso: ${response.data.id}`);
+
+            history.push('/');
+            
         } catch (err) {
             alert('Erro no cadastro, tente novamente.');
         }
