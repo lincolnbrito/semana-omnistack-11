ab02fade93582d9ba9db365a737fb3e64bac6fe0
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 17:08:05 -0300

    create Profile page

--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -15,7 +15,7 @@ export default function Register() {
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Já tennho cadastro
+                        Já tenho cadastro
                     </Link>
                 </section>
 
