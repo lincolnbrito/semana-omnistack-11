ca996d3f0f1fb3292b00ab0eaf80b9850532dfef
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 22:07:04 -0300

    list incidents

--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css'
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    useEffect(() => { 
+        api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [ongId]);
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Ben vindo, APAD</span>
+                <span>Ben vindo, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo incidente</Link>
                 <button type="button">
@@ -20,62 +36,23 @@ export default function Profile() {
 
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur accusamus eveniet, numquam temporibus omnis dolorem dolor natus soluta facere fugit aliquid hic distinctio tenetur ducimus porro deleniti. Mollitia, facilis! Vitae.</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.description}</p>
 
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALOR:</strong>
+                        <p>{incident.value}</p>
 
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur accusamus eveniet, numquam temporibus omnis dolorem dolor natus soluta facere fugit aliquid hic distinctio tenetur ducimus porro deleniti. Mollitia, facilis! Vitae.</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur accusamus eveniet, numquam temporibus omnis dolorem dolor natus soluta facere fugit aliquid hic distinctio tenetur ducimus porro deleniti. Mollitia, facilis! Vitae.</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur accusamus eveniet, numquam temporibus omnis dolorem dolor natus soluta facere fugit aliquid hic distinctio tenetur ducimus porro deleniti. Mollitia, facilis! Vitae.</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                        <button>
+                            <FiTrash2 size={20} color="#a8a8b3"/>
+                        </button>
+                    </li>
+                ))}
+                
             </ul>
         </div>
     );
