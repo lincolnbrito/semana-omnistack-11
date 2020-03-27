a6919c370d51f2daf2be2e75a2b3129e0b885b6f
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-27 01:56:35 -0300

    install expo-constants

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia</Text>
+        </View>
     );
 }
