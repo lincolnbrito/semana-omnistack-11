a1acb6ea340e31effdb10e502847bbce3aeadd41
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-27 01:44:36 -0300

    create pages structure and routes

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Semana OmniStack 11</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontSize: 20,
-    fontWeight: '500'
-  }
-});
