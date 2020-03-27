86dd6d066f3ebcc3269344c232ad3e7926994ac2
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-27 01:14:49 -0300

    :tada: create mobile structure

---
+++ aulas/mobile/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Semana OmniStack 11</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: '500'
+  }
+});
