8afb4d381576a8639901a573dbff53ae742c773a
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 22:39:39 -0300

    logout

--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -8,6 +8,8 @@ import './styles.css'
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const history = useHistory();
+
     const [incidents, setIncidents] = useState([]);
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -35,6 +37,13 @@ export default function Profile() {
             alert('Erro ao deletar caso, tente novamente');
         }
     }
+
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -42,7 +51,7 @@ export default function Profile() {
                 <span>Ben vindo, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo incidente</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041"/>
                 </button>
             </header>
