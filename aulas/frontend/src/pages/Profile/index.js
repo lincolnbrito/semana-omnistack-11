35db64430229c4999c05110bb86239f2efb47c90
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-26 22:31:28 -0300

    delete incidents

--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -22,6 +22,19 @@ export default function Profile() {
         })
     }, [ongId]);
 
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId
+                }
+            })
+
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        } catch (error) {
+            alert('Erro ao deletar caso, tente novamente');
+        }
+    }
     return (
         <div className="profile-container">
             <header>
@@ -45,9 +58,9 @@ export default function Profile() {
                         <p>{incident.description}</p>
 
                         <strong>VALOR:</strong>
-                        <p>{incident.value}</p>
+                        <p>{Intl.NumberFormat('pt-BR', {style:'currency',currency:'BRL'}).format(incident.value) }</p>
 
-                        <button>
+                        <button onClick={ () => handleDeleteIncident(incident.id) }>
                             <FiTrash2 size={20} color="#a8a8b3"/>
                         </button>
                     </li>
