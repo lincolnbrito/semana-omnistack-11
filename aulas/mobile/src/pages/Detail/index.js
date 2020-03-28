fea678c21a30729ed7467cd0eac8655c0463dd0d
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-27 22:10:01 -0300

    pssing incident via route

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { View, TouchableOpacity, Text, Image, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer'
 
@@ -11,6 +11,9 @@ import logoImg from '../../assets/logo.png';
 
 export default function Detail() {
     const navigation = useNavigation();
+    const route = useRoute();
+    const incident = route.params.incident;
+
     const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso: "Cadelinha atropelada" com o valor de R$ 120,00';
     
     function navigateBack() {
@@ -40,14 +43,19 @@ export default function Detail() {
             </View>
 
             <View style={styles.incident}>
-                <Text style={ [styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Lorem ipsum dolor sit amet.</Text>
+                <Text style={styles.incidentValue}>{incident.description}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(incident.value)}
+                </Text>
             </View>
 
             <View style={styles.contactBox}>
