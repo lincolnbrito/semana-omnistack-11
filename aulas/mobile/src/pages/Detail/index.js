e9a3f453832ca645c9e89f6aed8ae7397e054f86
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-27 22:17:35 -0300

    :tada: finish detail screen

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -14,7 +14,10 @@ export default function Detail() {
     const route = useRoute();
     const incident = route.params.incident;
 
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso: "Cadelinha atropelada" com o valor de R$ 120,00';
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso: "${incident.title}" com o valor de  ${Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(incident.value)}`;
     
     function navigateBack() {
         navigation.goBack();
@@ -22,14 +25,14 @@ export default function Detail() {
 
     function sendMail() {
         MailComposer.composeAsync({
-            subject: `Heróis do caso: Cadelinha atropelada`,
-            recipients: ['lincoln.sbrito@gmail.com'],
+            subject: `Heróis do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message
         });
     }
 
     function sendWhatsapp() {
-        Linking.openURL(`whatsapp://send?phone=5577999999878&text=${message}`);
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
     }
 
     return (
