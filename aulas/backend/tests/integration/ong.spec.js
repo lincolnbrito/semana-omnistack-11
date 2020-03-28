af685c1cf97fc96aa43db488cae6376ac2df2c85
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-03-28 16:44:32 -0300

    split app and server, create test connection and create test

---
+++ aulas/backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "APAD teste",
+                email: "apadteste@apad.org.br",
+                whatsapp: "55778888888",
+                city: "Vitória da Conquista",
+                uf: "BA"
+            });
+        
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+        
+    });
+})
